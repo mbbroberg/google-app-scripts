25b90e437f0ae61c24e8c4cfd0d002b54418008b
Matt Brender <mjbrender@gmail.com>
2015-10-18 12:46:41 -0400

    syntax error on JSON object
    Not sure how it got through in the first place, but found it on round two.

--- slack-push/slack-push.js
+++ slack-push/slack-push.js
@@ -1,9 +1,9 @@
 var REQ_SHEET = "Form Responses 1";
-var SLACK_API = "INSERT HERE";
+var SLACK_API = INSERT HERE;
 var BOT_ACTION = "Sent to Slack!";
 
 function myFunction() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(REQ_SHEET); // hard coding which spreadsheet 
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(REQ_SHEET); // hard coding which spreadsheet
   var data = sheet.getDataRange().getValues();                                 // helpful in case we add a second sheet for filtering
 
   for (var i = 0; i < data.length; ++i) {
@@ -13,11 +13,11 @@ function myFunction() {
     var opener = "*New Request!*\n\nThe following user wants to join WADE: ";
     var detail = "*Description:* ";
     var contactHow = row[1];             // column B is EMAIL address
-    var aboutMe = row[2];   // is column C 
+    var aboutMe = row[2];   // is column C
 
     var accessRequest = opener.concat(contactHow);
     var accessDetails = detail.concat(aboutMe);
-    
+
     var slackSent = row[3]; // column D -- Slack notes
     if (slackSent != BOT_ACTION) { // if we've never sent a notification to Slack, then:
       var url = SLACK_API;
@@ -30,11 +30,11 @@ function myFunction() {
       };
       var options = {
         'method': 'post',
-        'payload': 'payload=' + JSON.stringify(payload)
+        'payload': JSON.stringify(payload)
       };
       var response = UrlFetchApp.fetch(url, options);
 
-      sheet.getRange(1 + i, 4).setValue(BOT_ACTION); // reuse this! 
+      sheet.getRange(1 + i, 4).setValue(BOT_ACTION); // reuse this!
       // Make sure the cell is updated right away in case the script is interrupted
       SpreadsheetApp.flush();
     } // end if -- out of here when we're done sending things out to slack
